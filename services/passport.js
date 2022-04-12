4fb3778ecb66ed57c32c95a7955af2846fc74a89
Niraj Lekhak <nrajlekhak@gmail.com>
2022-04-12 14:07:11 +0545

    added authentication

---
+++ services/passport.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        console.log('User already exists, logging in user');
+        done(null, existingUser);
+      } else {
+        console.log('creating new user');
+        const newUser = await new User({
+          googleId: profile.id,
+        }).save();
+        done(null, newUser);
+      }
+    }
+  )
+);
